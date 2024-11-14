b193693d7004f05c898b049f5d76e257e492d6a7
Lauryneodarih <lauryneodarih@gmail.com>
2024-11-13 22:21:59 -0600

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,329 @@
+########################### DO NOT MODIFY THIS SECTION ##########################
+#################################################################################
+import sqlite3
+from sqlite3 import Error
+import csv
+#################################################################################
+
+## Change to False to disable Sample
+SHOW = False
+
+############### SAMPLE CLASS AND SQL QUERY ###########################
+######################################################################
+class Sample():
+    def sample(self):
+        try:
+            connection = sqlite3.connect("sample.sqlite3")
+            connection.text_factory = str
+        except Error as e:
+            print("Error occurred: " + str(e))
+        print('\033[32m' + "Sample: " + '\033[m')
+        
+        # Sample Drop table
+        connection.execute("DROP TABLE IF EXISTS sample;")
+        # Sample Create
+        connection.execute("CREATE TABLE sample(id integer, name text);")
+        # Sample Insert
+        connection.execute("INSERT INTO sample VALUES (?,?)",("1","test_name"))
+        connection.commit()
+        # Sample Select
+        cursor = connection.execute("SELECT * FROM sample;")
+        print(cursor.fetchall())
+
+######################################################################
+
+class HW2_sql():
+    ############### DO NOT MODIFY THIS SECTION ###########################
+    ######################################################################
+    def create_connection(self, path):
+        connection = None
+        try:
+            connection = sqlite3.connect(path)
+            connection.text_factory = str
+        except Error as e:
+            print("Error occurred: " + str(e))
+    
+        return connection
+
+    def execute_query(self, connection, query):
+        cursor = connection.cursor()
+        try:
+            if query == "":
+                return "Query Blank"
+            else:
+                cursor.execute(query)
+                connection.commit()
+                return "Query executed successfully"
+        except Error as e:
+            return "Error occurred: " + str(e)
+    ######################################################################
+    ######################################################################
+
+    # GTusername [0 points]
+    def GTusername(self):
+        gt_username = "lodarih3"
+        return gt_username
+    
+    # Part a.i Create Tables [2 points]
+    def part_ai_1(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+
+        part_ai_1_sql = "create table if not exists movies( id integer PRIMARY KEY,title text, core real);"
+        ######################################################################
+        
+        return self.execute_query(connection, part_ai_1_sql)
+
+    def part_ai_2(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_ai_2_sql = "create table if not exists  movie_cast( movie_id integer,cast_id integer, cast_name text,birthday text, popularity real);"
+        ######################################################################
+        
+        return self.execute_query(connection, part_ai_2_sql)
+    
+    # Part a.ii Import Data [2 points]
+    def part_aii_1(self,connection,path):
+        ############### CREATE IMPORT CODE BELOW ############################
+
+        with open('./data/movies.csv', newline='') as csvfile:
+
+            spamreader = csv.reader(csvfile)
+
+            for row in spamreader:
+                id, title, core = row
+                connection.execute("INSERT INTO  movies VALUES (?,?,?)",(int(id),title,core))
+                connection.commit()
+       ######################################################################
+        
+        sql = "SELECT COUNT(id) FROM movies;"
+        cursor = connection.execute(sql)
+        return cursor.fetchall()[0][0]
+    
+    def part_aii_2(self,connection, path):
+        ############### CREATE IMPORT CODE BELOW ############################
+        with open('./data/movie_cast.csv', newline='') as csvfile:
+
+            spamreader = csv.reader(csvfile)
+
+            for row in spamreader:
+                    movie_id, cast_id, cast_name, birthday, popularity = row
+                    connection.execute("INSERT INTO  movie_cast VALUES (?,?,?,?,?)",(int(movie_id),int(cast_id),cast_name,birthday,popularity))
+                    connection.commit()
+               
+        ######################################################################
+        
+        sql = "SELECT COUNT(cast_id) FROM movie_cast;"
+        cursor = connection.execute(sql)
+        return cursor.fetchall()[0][0]
+
+    # Part a.iii Vertical Database Partitioning [5 points]
+    def part_aiii(self,connection):
+        ############### EDIT CREATE TABLE SQL STATEMENT ###################################
+        part_aiii_sql = "create table if not exists  cast_bio(cast_id integer ,movie_cast text, cast_name text,birthday text,popularity);"
+        ######################################################################
+        
+        self.execute_query(connection, part_aiii_sql)
+        
+        ############### CREATE IMPORT CODE BELOW ############################
+        part_aiii_insert_sql = "INSERT INTO cast_bio(cast_id,cast_name,birthday,popularity)SELECT er.cast_id,er.cast_name,er.birthday, er.popularity FROM movie_cast er GROUP BY cast_id;"
+        ######################################################################
+        
+        self.execute_query(connection, part_aiii_insert_sql)
+        
+        sql = "SELECT COUNT(cast_id) FROM cast_bio;"
+        cursor = connection.execute(sql)
+        return cursor.fetchall()[0][0]
+       
+
+    # Part b Create Indexes [1 points]
+    def part_b_1(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_b_1_sql = "CREATE INDEX movie_index ON movies(id)"
+        ######################################################################
+        return self.execute_query(connection, part_b_1_sql)
+    
+    def part_b_2(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_b_2_sql = "CREATE INDEX cast_index ON movie_cast (cast_id)"
+        ######################################################################
+        return self.execute_query(connection, part_b_2_sql)
+    
+    def part_b_3(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_b_3_sql = "CREATE INDEX cast_bio_index ON cast_bio (cast_id)"
+        ######################################################################
+        return self.execute_query(connection, part_b_3_sql)
+    
+    # Part c Calculate a Proportion [3 points]
+    def part_c(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_c_sql = "WITH total_actors AS (SELECT COUNT(*) as count FROM movie_cast WHERE birthday != 'None'),actors_born_in_range AS (SELECT COUNT(*) as count FROM movie_cast WHERE birthday != 'None' AND birthday BETWEEN '1965-01-01' AND '1985-12-31') SELECT (actors_born_in_range.count * 100.0) / total_actors.count AS proportion FROM total_actors, actors_born_in_range"
+        ######################################################################
+        cursor = connection.execute(part_c_sql)
+        return cursor.fetchall()[0][0]
+
+    # Part d Find the Most Prolific Actors [4 points]
+    def part_d(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_d_sql = "SELECT cast_name,COUNT(movie_id)FROM movie_cast WHERE popularity > 10 GROUP BY cast_name ORDER BY COUNT(movie_id) DESC,cast_name LIMIT 5"
+        ######################################################################
+        cursor = connection.execute(part_d_sql)
+        return cursor.fetchall()
+
+    # Part e Find the Highest Scoring Movies With the Least Amount of Cast [4 points]
+    def part_e(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_e_sql = "SELECT title,printf('%.2f',score) AS movie_score,COUNT(cast_id) as cast_count from movies INNER JOIN movie_cast on movie_cast.movie_id=movies.id GROUP By movies.id ORDER BY score DESC, cast_count ASC,title ASC LIMIT 5"
+        ######################################################################
+        cursor = connection.execute(part_e_sql)
+        return cursor.fetchall()
+    
+    # Part f Get High Scoring Actors [4 points]
+    def part_f(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_f_sql = "select cast_id,cast_name As cast_name,printf('%.2f',avg(score)) FROM movie_cast INNER JOIN movies on movie_cast.movie_id=movies.id where movies.score>25 GROUP by cast_id HAVING COUNT(movie_id)>2 ORDER BY avg(score) DESC,cast_name ASC LIMIT 10"
+        ######################################################################
+        cursor = connection.execute(part_f_sql)
+        return cursor.fetchall()
+
+    # Part g Creating Views [6 points]
+    def part_g(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_g_sql = "Create VIEW good_collaboration AS SELECT a.cast_id as cast_member_id1,b.cast_id As cast_member_id2,COUNT(a.movie_id) AS movie_count,printf('%.2f',avg(c.score)) AS average_movie_score From movie_cast a,movie_cast b,movies c where a.movie_id =b.movie_id And a.cast_id <> b.cast_id and a.movie_id=c.id GROUP By a.cast_id,b.cast_id HAVING COUNT(a.movie_id)>2 and avg(c.score)>=40"
+        ######################################################################
+        return self.execute_query(connection, part_g_sql)
+    
+    def part_gi(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_g_i_sql = "SELECT cast_member_id1,cast_member_id2,movie_count,average_movie_score as collaboration_score FROM good_collaboration order By average_movie_score DESC LIMIT 5"
+        ######################################################################
+        cursor = connection.execute(part_g_i_sql)
+        return cursor.fetchall()
+    
+    # Part h FTS [4 points]
+    def part_h(self,connection,path):
+        ############### EDIT SQL STATEMENT ###################################
+        part_h_sql = "CREATE VIRTUAL TABLE movie_overview USING fts4(id INTEGER, overview TEXT)"
+        ######################################################################
+        connection.execute(part_h_sql)
+        ############### CREATE IMPORT CODE BELOW ############################
+        with open('./data/movie_overview.csv',errors = 'ignore') as csvfile:
+            file = csv.reader(csvfile,delimiter = ',')
+            for row in file:
+                connection.execute("INSERT INTO movie_overview values (?,?)",
+                (row))
+                connection.commit()
+
+
+        ######################################################################
+        sql = "SELECT COUNT(id) FROM movie_overview"
+        cursor = connection.execute(sql)
+        return cursor.fetchall()[0][0]
+        
+    def part_hi(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_hi_sql = "SELECT COUNT(id) as count_overview FROM movie_overview WHERE overview MATCH 'fight'"
+        ######################################################################
+        cursor = connection.execute(part_hi_sql)
+        return cursor.fetchall()[0][0]
+    
+    def part_hii(self,connection):
+        ############### EDIT SQL STATEMENT ###################################
+        part_hii_sql = "SELECT COUNT(id) FROM movie_overview WHERE overview MATCH 'space NEAR 5 program'"
+        ######################################################################
+        cursor = connection.execute(part_hii_sql)
+        return cursor.fetchall()[0][0]
+
+
+if __name__ == "__main__":
+    
+    ########################### DO NOT MODIFY THIS SECTION ##########################
+    #################################################################################
+    if SHOW == True:
+        sample = Sample()
+        sample.sample()
+
+    print('\033[32m' + "Q2 Output: " + '\033[m')
+    db = HW2_sql()
+    try:
+        conn = db.create_connection("Q2.sqlite")
+    except:
+        print("Database Creation Error")
+
+    try:
+        conn.execute("DROP TABLE IF EXISTS movies;")
+        conn.execute("DROP TABLE IF EXISTS movie_cast;")
+        conn.execute("DROP TABLE IF EXISTS cast_bio;")
+        conn.execute("DROP VIEW IF EXISTS good_collaboration;")
+        conn.execute("DROP TABLE IF EXISTS movie_overview;")
+    except:
+        print("Error in Table Drops")
+
+    try:
+        print('\033[32m' + "part ai 1: " + '\033[m' + str(db.part_ai_1(conn)))
+        print('\033[32m' + "part ai 2: " + '\033[m' + str(db.part_ai_2(conn)))
+    except:
+         print("Error in Part a.i")
+
+    try:
+        print('\033[32m' + "Row count for Movies Table: " + '\033[m' + str(db.part_aii_1(conn,"data/movies.csv")))
+        print('\033[32m' + "Row count for Movie Cast Table: " + '\033[m' + str(db.part_aii_2(conn,"data/movie_cast.csv")))
+    except:
+        print("Error in part a.ii")
+
+    try:
+        print('\033[32m' + "Row count for Cast Bio Table: " + '\033[m' + str(db.part_aiii(conn)))
+    except:
+        print("Error in part a.iii")
+
+    try:
+        print('\033[32m' + "part b 1: " + '\033[m' + db.part_b_1(conn))
+        print('\033[32m' + "part b 2: " + '\033[m' + db.part_b_2(conn))
+        print('\033[32m' + "part b 3: " + '\033[m' + db.part_b_3(conn))
+    except:
+        print("Error in part b")
+
+    try:
+        print('\033[32m' + "part c: " + '\033[m' + str(db.part_c(conn)))
+    except:
+        print("Error in part c")
+
+    try:
+        print('\033[32m' + "part d: " + '\033[m')
+        for line in db.part_d(conn):
+            print(line[0],line[1])
+    except:
+        print("Error in part d")
+
+    try:
+        print('\033[32m' + "part e: " + '\033[m')
+        for line in db.part_e(conn):
+            print(line[0],line[1],line[2])
+    except:
+        print("Error in part e")
+
+    try:
+        print('\033[32m' + "part f: " + '\033[m')
+        for line in db.part_f(conn):
+            print(line[0],line[1],line[2])
+    except:
+        print("Error in part f")
+    
+    try:
+        print('\033[32m' + "part g: " + '\033[m' + str(db.part_g(conn)))
+        print('\033[32m' + "part g.i: " + '\033[m')
+        for line in db.part_gi(conn):
+            print(line[0],line[1],line[2])
+    except:
+        print("Error in part g")
+
+    try:   
+        print('\033[32m' + "part h.i: " + '\033[m'+ str(db.part_h(conn,"data/movie_overview.csv")))
+        print('\033[32m' + "Count h.ii: " + '\033[m' + str(db.part_hi(conn)))
+        print('\033[32m' + "Count h.iii: " + '\033[m' + str(db.part_hii(conn)))
+    except:
+        print("Error in part h")
+
+    conn.close()
+    #################################################################################
+    #################################################################################
+  
